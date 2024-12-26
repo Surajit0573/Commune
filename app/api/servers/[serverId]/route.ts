40c4b76a37d8d664a508454874c8e7aab66c4f1e
Surajit Maity <sibu23122003@gmail.com>
2024-12-27 04:58:43 +0530

    Delete Server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -5,27 +5,52 @@ import { NextResponse } from "next/server";
 export async function PATCH(
     req: Request, {
         params }: { params: { serverId: string } }) {
-try{
+    try {
 
-    const profile=await currentProfile();
-    const {name,imageUrl} =await req.json();
-    if(!profile){
-        return new NextResponse("Unauthorized",{status:401});
-    }
-
-    const server=await db.server.update({
-        where:{
-            id:params.serverId,
-            profileId:profile.id // only a admin can modify server settings
-        },
-        data: {
-            name,imageUrl
+        const profile = await currentProfile();
+        const { name, imageUrl } = await req.json();
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }
-    })
 
-    return NextResponse.json(server);
-}catch(error){
-    console.log("SERVER_ID_PATCH ",error);
-    return new NextResponse("Internal Error",{status:500})
+        const server = await db.server.update({
+            where: {
+                id: params.serverId,
+                profileId: profile.id // only a admin can modify server settings
+            },
+            data: {
+                name, imageUrl
+            }
+        })
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("SERVER_ID_PATCH ", error);
+        return new NextResponse("Internal Error", { status: 500 })
+    }
 }
+
+export async function DELETE(
+    req: Request, {
+        params }: { params: { serverId: string } }) {
+    try {
+
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id // only a admin can delete a server
+            }
+        })
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("SERVER_ID_DELETE ", error);
+        return new NextResponse("Internal Error", { status: 500 })
+    }
 }
