59764529771d864fde41f55a38673cda5c162f4b
Surajit Maity <sibu23122003@gmail.com>
2024-12-30 05:13:56 +0530

    infinite query of chat for realtime feel, even is socket.io is failed to connect

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,58 +1,71 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessage } from "@/components/chat/chat-message";
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 
-interface ChannelIdPageProps{
-    params:{
-        serverId:string,
-        channelId:string
+interface ChannelIdPageProps {
+    params: {
+        serverId: string,
+        channelId: string
     }
 }
 
-export default async ({params}:ChannelIdPageProps)=>{
+export default async ({ params }: ChannelIdPageProps) => {
 
-    const profile=await currentProfile();
-    if(!profile){
+    const profile = await currentProfile();
+    if (!profile) {
         return redirect("/");
     }
 
-    const channel=await db.channel.findUnique({
-        where:{
-            id:params.channelId
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId
         }
     })
 
-    const member=await db.channel.findFirst({
-        where:{
-            serverId:params.serverId,
-            profileId:profile.id
-        }
-    }) 
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id
+        },
+    })
 
-    if(!channel||!member){
+    if (!channel || !member) {
         redirect("/");
     }
 
-    return(
+    return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader
-            name={channel.name}
-            serverId={channel.serverId}
-            type="channel"
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
             />
-            <div className="flex-1">
-                Future Messages
-            </div>
+            <ChatMessage
+                member={member}
+                name={channel.name}
+                chatId={channel.id}
+                type="channel"
+                apiUrl="/api/messages"
+                socketUrl="/api/socket/messages"
+                socketQuery={{
+                    channelId: channel.id,
+                    serverId: channel.serverId
+                }}
+                paramKey="channelId"
+                paramValue={channel.id}
+
+            />
             <ChatInput
-            name={channel.name}
-            type="channel"
-            apiUrl="/api/socket/messages"
-            query={{
-                channelId:channel.id,
-                serverId:channel.serverId,
-            }}
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
             />
         </div>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,7 +42,9 @@ export default function RootLayout({
              storageKey="discord-theme">
               <SocketProvider>
             <ModalProvider/>
+            <QueryProvider>
             {children}
+            </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
